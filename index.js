786f6feea3ef156b0c33b61efeba6f46d52febb9
diana1002 <diana.popa1002@gmail.com>
2020-01-05 11:53:28 +0200

    The reducer function

--- index.js
+++ index.js
@@ -30,6 +30,14 @@
   id: 0
 }
 
+function todos (state = [], action) {
+  if (action.type === 'ADD_TODO') {
+    return state.concat([action.todo])
+  }
+
+  return state
+}
+
 function createStore () {
   let state
   let listeners = []
